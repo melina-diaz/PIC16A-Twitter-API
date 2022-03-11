41f6bb5f0b24910c910f3926f66e67299b27250b
Tracy Charles <81223941+tracycharles108@users.noreply.github.com>
2022-03-11 15:01:22 -0800

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -1,4 +1,10 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+from wordcloud import WordCloud
+from textblob import TextBlob
+import re
+import numpy as np
+
 class Analysis:
     def __init__(self, csv_name):
         '''Makes '''
@@ -36,8 +42,77 @@ class Analysis:
         '''Purpose:
         Arguments: 
         Return: '''
-        #Tracy's code, feel free to change whatever
-        return 
+
+        # get new subset dataframe 
+        df1 = self.df[['created_at_x', 'text']]
+
+        # define function to create sentiment column using TextBlob
+        def get_sentiment(text):
+            if TextBlob(text).sentiment.polarity > 0:
+                return 'Positive'
+            elif TextBlob(text).sentiment.polarity < 0:
+                return 'Negative'
+            else:
+                return 'Neutral'
+
+        # create 'sentiment' column for df1
+        df1['sentiment'] = df1['text'].apply(get_sentiment)
+
+        # create groupedby table
+        groupedby_sentiment = df1.groupby('sentiment').size()
+
+        # create bar graph for sentiments
+        fig, ax = plt.subplots(1, figsize = (5, 5))
+        ax.bar(groupedby_sentiment.index, groupedby_sentiment, color = ['red', 'grey', 'green'])
+        ax.set_xlabel('Sentiment')
+        ax.set_ylabel('Count')
+        fig.suptitle('Sentiment Analysis of Tweets')
+
+        # define function to add number labels on graph
+        def addlabels(x,y):
+            for i in range(len(x)):
+                ax.text(i, y[i], str((100 * y[i] / len(df1)).round(2)) + " %", ha = 'center')
+        addlabels(groupedby_sentiment.index, groupedby_sentiment)
+
+        # define function to clean each sentiment text
+        def get_cleaned_text(df1, sentiment):
+            out = df1.groupby('sentiment').get_group(sentiment)
+            out = out['text']
+            out = " ".join(x for x in out)
+            # get rid of links
+            out = re.sub('https://t.co\S*\w+|\n', ' ', out)
+            return out
+
+        # get text of each sentiment
+        negative_text = get_cleaned_text(df1, 'Negative')
+        neutral_text = get_cleaned_text(df1, 'Neutral')
+        positive_text = get_cleaned_text(df1, 'Positive')
+
+        # define functions for color scheme in word clouds
+        def red_color_func(word, font_size, position, orientation, random_state = None, **kwargs):
+            return("hsl(0, 100%%, %d%%)" % np.random.randint(10, 70))
+
+        def grey_color_func(word, font_size, position, orientation, random_state = None, **kwargs):
+            return("hsl(0, 0%%, %d%%)" % np.random.randint(10, 70))
+
+        def green_color_func(word, font_size, position, orientation, random_state = None, **kwargs):
+            return("hsl(75, 100%%, %d%%)" % np.random.randint(10, 50))
+
+        # define function to create word clouds
+        def get_word_cloud(text, func, title):
+            wordcloud = WordCloud(background_color = 'white').generate(text)
+            wordcloud.recolor(color_func = func)
+            plt.figure(figsize = (15, 15))
+            plt.imshow(wordcloud, interpolation = 'bilinear')
+            plt.axis("off")
+            plt.title(title)
+            return plt
+
+        negative_word_cloud = get_word_cloud(negative_text, red_color_func, 'Word Cloud of Negative Tweets')
+        neutral_word_cloud = get_word_cloud(neutral_text, grey_color_func, 'Word Cloud of Neutral Tweets')
+        positive_word_cloud = get_word_cloud(neutral_text, green_color_func, 'Word Cloud of Positive Tweets')
+
+        return fig, negative_word_cloud, neutral_word_cloud, positive_word_cloud
     
     def predict(self):
         '''Purpose:
