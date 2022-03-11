ac0d81199fc9768244f256fb5c0abea437c2baed
blackshield-01 <100242193+blackshield-01@users.noreply.github.com>
2022-03-11 14:22:04 -0800

    Update Analysis.py

--- Analysis.py
+++ Analysis.py
@@ -10,7 +10,24 @@ class Analysis:
         '''Purpose:
         Arguments: 
         Return: '''
-        #Michelle's code, feel free to change whatever
+        from sklearn.feature_extraction.text import CountVectorizer
+        vec = CountVectorizer(stop_words="english")
+        counts = vec.fit_transform(tweetsandusers['text'])
+        counts = counts.toarray()
+        vec.get_feature_names()
+        count_tweetsandusers = pd.DataFrame(counts, columns=vec.get_feature_names())
+        big_count_tweetsandusers = pd.concat((tweetsandusers["text"],count_tweetsandusers), axis= 1)
+        X = big_count_tweetsandusers.drop(columns = "text")
+        from sklearn.decomposition import NMF
+        model = NMF(n_components = 10, init="random", random_state=0)
+        model.fit(X)
+        import numpy as np
+        def top_words(X, model, component, num_words):
+            orders = np.argsort(model.components_, axis = 1)
+            important_words = np.array(X.columns)[orders]
+            return important_words[component][-num_words:]
+        for i in range (10):
+            print(top_words(X, model, i, 4))
         return
     
     def get_sentiment():
