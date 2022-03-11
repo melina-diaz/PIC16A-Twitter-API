c5e71e5f82ed05a4e80e39764e5e1250e515926d
melina-diaz <82481505+melina-diaz@users.noreply.github.com>
2022-03-11 14:36:48 -0800

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -1,10 +1,12 @@
+import pandas as pd
 class Analysis:
-    def __init__(csv_name):
+    def __init__(self, csv_name):
         '''Makes '''
         if type(csv_name) != str:
             raise TypeError("csv_name should be a string, specifically the path to the csv of tweets")
         self.csv=csv_name
         self.df=pd.read_csv(csv_name) #you shouldn't alter the original df
+        self.df=self.df.drop(columns=["Unnamed: 0"])
         
     def get_topics(self):
         '''Purpose:
@@ -12,11 +14,11 @@ class Analysis:
         Return: '''
         from sklearn.feature_extraction.text import CountVectorizer
         vec = CountVectorizer(stop_words="english")
-        counts = vec.fit_transform(tweetsandusers['text'])
+        counts = vec.fit_transform(self.df['text'])
         counts = counts.toarray()
         vec.get_feature_names()
         count_tweetsandusers = pd.DataFrame(counts, columns=vec.get_feature_names())
-        big_count_tweetsandusers = pd.concat((tweetsandusers["text"],count_tweetsandusers), axis= 1)
+        big_count_tweetsandusers = pd.concat((self.df["text"],count_tweetsandusers), axis= 1)
         X = big_count_tweetsandusers.drop(columns = "text")
         from sklearn.decomposition import NMF
         model = NMF(n_components = 10, init="random", random_state=0)
@@ -54,6 +56,6 @@ class Analysis:
         new["popularity"]= self.df["retweet_count_x"]+ self.df["reply_count_x"]+ self.df["like_count_x"]
         new=new.sort_values("popularity", ascending=False).reset_index()
         for i in range(num):
-            print("\033[1m#" + str(i) +" with " + str(new["popularity"][i]) + " popularity score: \033[0m")
-            print(new["text"][i] +"\n")
+            print("\033[1m#" + str(i+1) +" with " + str(new["popularity"][i+1]) + " popularity score: \033[0m")
+            print(new["text"][i+1] +"\n")
         
