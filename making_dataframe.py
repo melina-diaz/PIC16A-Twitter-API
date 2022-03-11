fb460ab42cc9a484a54251560dfa942b0299ed60
melina-diaz <82481505+melina-diaz@users.noreply.github.com>
2022-03-10 22:44:11 -0800

    Fixed dictionary problem, added docstring

--- making_dataframe.py
+++ making_dataframe.py
@@ -15,23 +15,26 @@ import unicodedata
 #To add wait time between requests
 import time
 
-
+#the following code is from https://towardsdatascience.com/an-extensive-guide-to-collecting-tweets-from-twitter-api-v2-for-academic-research-using-python-3-518fcb71df2a
 def create_headers(bearer_token):
     headers = {"Authorization": "Bearer {}".format(bearer_token)}
     return headers
 
 def create_url(keyword, start_date, end_date, max_results = 100):
-    
     search_url = "https://api.twitter.com/2/tweets/search/recent" #Change to the endpoint you want to collect data from
 
-#CHANGE THIS PART TO DISPLAY DIFFERENT VALUES
+#CHANGE THIS PART TO DISPLAY DIFFERENT VALUES. Next to each field is all possible options
     query_params = {'query': keyword,
                     'max_results': max_results,
-                    'expansions': 'author_id,geo.place_id', #'author_id,in_reply_to_user_id,geo.place_id', expansions controls which of the below fields you want to see
+                    # expansions is if you want the author, geo, etc. attributes included below to show up in the JSON
+                    'expansions': 'author_id,geo.place_id', #'author_id,in_reply_to_user_id,geo.place_id'
+                    # attributes of the tweets
                     'tweet.fields': 'text,public_metrics,referenced_tweets,geo,created_at', #'id,text,author_id,in_reply_to_user_id,geo,conversation_id,created_at,lang,public_metrics,referenced_tweets,reply_settings,source',
+                    # attributes of the users
                     'user.fields': 'name,username,created_at,description,public_metrics,verified', #'id,name,username,created_at,description,public_metrics,verified',
+                    # attributes of the places
                     'place.fields': 'full_name,country,country_code,geo,name,place_type', #'full_name,id,country,country_code,geo,name,place_type',
-                    'next_token': {}}
+                    'next_token': {}} #if you have a next_token put it here
     return (search_url, query_params)
 
 def connect_to_endpoint(url, headers, params, next_token = None):
@@ -43,6 +46,17 @@ def connect_to_endpoint(url, headers, params, next_token = None):
     return response.json()
 
 def make_call(bearer_token, keyword="ucla -is:retweet lang:en", start_time = "2022-02-01T00:00:00.000Z", end_time = "2022-02-28T00:00:00.000Z", max_results = 100):
+    '''
+    Purpose: Calls the appropriate functions to turn the tweet data string from the API into 
+    a JSON, which is then converted into a CSV file on computer
+    
+    Arguments:
+        bearer_token: the token from your Twitter API Developer account to use your API
+        keyword: the criteria that the tweet/user/geo/etc needs to fulfill in order to be returned. operators you can use https://developer.twitter.com/en/docs/twitter-api/tweets/search/integrate/build-a-query
+        start_time, end_time: I honestly don't know what this does, doesn't affect results as far as I can tell
+        max_results: tweets returned, int [10, 100]
+        
+    '''
     headers = create_headers(bearer_token)
     url = create_url(keyword, start_time,end_time, max_results)
     json_response = connect_to_endpoint(url[0], headers, url[1])
@@ -53,11 +67,14 @@ def make_call(bearer_token, keyword="ucla -is:retweet lang:en", start_time = "20
     users = pd.DataFrame(file["includes"]["users"])
     places = pd.DataFrame(file["includes"]["places"])
     users["author_id"]=users["id"]
-    tweets["retweet_count"] = tweets["public_metrics"].map(lambda x:x["retweet_count"])
-    tweets["reply_count"] = tweets["public_metrics"].map(lambda x:x["reply_count"])
-    tweets["like_count"] = tweets["public_metrics"].map(lambda x:x["like_count"])
+    tweets["retweet_count_x"] = tweets["public_metrics"].map(lambda x:x["retweet_count"])
+    tweets["reply_count_x"] = tweets["public_metrics"].map(lambda x:x["reply_count"])
+    tweets["like_count_x"] = tweets["public_metrics"].map(lambda x:x["like_count"])
+    users["followers_count_y"] = users["public_metrics"].map(lambda x:x["followers_count"])
+    users["following_count_y"] = users["public_metrics"].map(lambda x:x["following_count"])
+    users["tweet_count_y"] = users["public_metrics"].map(lambda x:x["tweet_count"])
+    users["listed_count_y"] = users["public_metrics"].map(lambda x:x["listed_count"])
     df=pd.merge(tweets,users, on="author_id")
     df.to_csv('uclatweets.csv')
     
-#make_call('')
-#how to run this in the command line....
+#make_call('put token hereeeeeee')
